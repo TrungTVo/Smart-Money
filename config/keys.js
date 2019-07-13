87f80dff19192f84c13f541670d255bda9a88fa0
trungvo <vtt311096@gmail.com>
2019-07-13 02:09:03 -0500

    updated

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,7 @@
-// var dotenv = require('dotenv');
-// dotenv.config();
+var dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
-  mongoURI: //process.env.NODE_ENV === 'production' ? process.env.MONGO_URI :
-   "mongodb+srv://trung:Ngosilien123@cluster0-r21n2.mongodb.net/smartMoney?retryWrites=true&w=majority",
-  secretOrKey: //process.env.NODE_ENV === 'production' ? process.env.SECRET_OR_KEY :
-   "secret"
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
 }
